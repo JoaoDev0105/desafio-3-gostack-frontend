026d5108dedf954479da9e652c7b6a4996411768
JoaoDev0105 <joaopaulo_dev@outlook.com>
2020-11-23 06:30:38 -0300

    Incrementando a criação e listando os repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
 
 function App() {
+  // Cria duas variaves para estado inicial e estado alterado dentro de um array vazio
+  const [repositories, setRepositories] = useState([]);
+  // Para buscar os projetos dentro do response.data através da função then 
+  useEffect (() => {
+    api.get(repositories).then(response => {
+      setRepositories(response.data)
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+      // Copiando todo o array através do express operation
+       // setProjects ([...projects, `Novo projeto ${Date.now()}`]);  
+       const response = await api.post('repositories', {
+        url: "https://github.com/joaopaulomp0105",
+        title: "Desafio ReactJS 3",
+        techs: ["HTML", "Node.js"],
+    });
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +30,12 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repository =>
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
